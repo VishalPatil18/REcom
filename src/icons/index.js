81519bd0b2e428e8b57884ed00b49baa89e0085c
VishalPatil18 <vishalpatil.imp@gmail.com>
2022-04-09 11:40:37 +0530

    FEAT: Add Icons library

---
+++ src/icons/index.js
@@ -0,0 +1,68 @@
+import { BiEnvelope } from "react-icons/bi";
+import { AiOutlineAppstore } from "react-icons/ai";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaHeart,
+  FaInstagram,
+  FaLinkedinIn,
+  FaMediumM,
+  FaRegHeart,
+  FaTwitter,
+} from "react-icons/fa";
+import {
+  FiBell,
+  FiBookmark,
+  FiEdit3,
+  FiImage,
+  FiLink2,
+  FiMessageCircle,
+  FiMessageSquare,
+  FiMoreHorizontal,
+  FiMoreVertical,
+  FiSearch,
+  FiSettings,
+  FiTrash2,
+  FiUser,
+  FiUserCheck,
+  FiUserPlus,
+  FiUsers,
+  FiUserX,
+  FiX,
+} from "react-icons/fi";
+
+export {
+  /****** UTILITY ******/
+  AiOutlineAppstore as Apps,
+  BiEnvelope as Message,
+  FaRegHeart as Like,
+  FiBell as Notification,
+  FiBookmark as Bookmark,
+  FiEdit3 as Pencil,
+  FiImage as Image,
+  FiLink2 as Link,
+  FiMessageCircle as Comment,
+  FiMessageSquare as Chat,
+  FiMoreHorizontal as Hmenu,
+  FiMoreVertical as Vmenu,
+  FiSearch as Search,
+  FiSettings as Settings,
+  FiTrash2 as Delete,
+  FiUser as User,
+  FiUserCheck as AddedFriend,
+  FiUserPlus as AddFriend,
+  FiUsers as Users,
+  FiUserX as RemoveFriend,
+  FiX as Close,
+
+  /****** SOLID ******/
+  FaHeart as Liked,
+  
+  /****** BRANDS ******/
+  FaFacebookF as Facebook,
+  FaGithub as Github,
+  FaInstagram as Instagram,
+  FaLinkedinIn as LinkedIn,
+  FaMediumM as Medium,
+  FaTwitter as Twitter,
+};
